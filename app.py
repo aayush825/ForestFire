9634d7790e79fbd6aba56046ad5a6234e0c0bbf4
aayush825 <ayushkumar250801@gmail.com>
2026-01-25 01:22:10 +0530

    test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 
 
-## import ridge regresor model and standard scaler pickle
+## import ridge regresor model and standard scaler pickle  changesssssss
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
